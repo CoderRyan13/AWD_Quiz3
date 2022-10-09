52cb348015ae99c116a2ce90a3363f0ee424557c
Ryan Armstrong <armstrongryan11@gmail.com>
2022-10-09 12:43:39 -0600

    Added code to start off final project

---
+++ cmd/api/photosharing.go
@@ -0,0 +1,82 @@
+// Filename: cmd/api/schools.go
+
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"time"
+
+	"AWD_FinalProject.ryanarmstrong.net/internal/data"
+	"AWD_FinalProject.ryanarmstrong.net/internal/validator"
+)
+
+// createSchoolHandler for the "Post /v1/schools" endpoint
+func (app *application) createSchoolHandler(w http.ResponseWriter, r *http.Request) {
+	// Our target decode destination
+	var input struct {
+		Name    string   `json:"name"`
+		Level   string   `json:"level"`
+		Contact string   `json:"contact"`
+		Phone   string   `json:"phone"`
+		Email   string   `json:"email"`
+		Website string   `json:"website"`
+		Address string   `json:"address"`
+		Mode    []string `json:"mode"`
+	}
+	// Initialize a new json.Decoder instance
+	err := app.readJSON(w, r, &input)
+	if err != nil {
+		app.badRequestResponse(w, r, err)
+		return
+	}
+
+	// Copy the values from the input struct to a new School struct
+	school := &data.School{
+		Name:    input.Name,
+		Level:   input.Level,
+		Contact: input.Contact,
+		Phone:   input.Phone,
+		Email:   input.Email,
+		Website: input.Website,
+		Address: input.Address,
+		Mode:    input.Mode,
+	}
+	// Initialize a new Validator instance
+	v := validator.New()
+
+	// Check the map to determine if there were any validation errors
+	if data.ValidateSchool(v, school); !v.Valid() {
+		app.failedValidationResponse(w, r, v.Errors)
+		return
+	}
+	// Display the request
+	fmt.Fprintf(w, "%+v\n", input)
+}
+
+// showSchoolHandler for the "Post /v1/schools/:id" endpoint
+func (app *application) showSchoolHandler(w http.ResponseWriter, r *http.Request) {
+	id, err := app.readIDParam(r)
+	if err != nil {
+		app.notFoundResponse(w, r)
+		return
+	}
+
+	// Create a new instance of the School struct containing the ID we extracted
+	// from our URL and some sample data
+	school := data.School{
+		ID:        id,
+		CreatedAt: time.Now(),
+		Name:      "Apple Tree",
+		Level:     "High School",
+		Contact:   "Anna Smith",
+		Phone:     "636-3636",
+		Address:   "13 Apple street",
+		Mode:      []string{"blended", "online"},
+		Version:   1,
+	}
+	err = app.writeJSON(w, http.StatusOK, envelope{"school": school}, nil)
+	if err != nil {
+		app.serverErrorResponse(w, r, err)
+	}
+}
---
+++ go.mod
@@ -0,0 +1,5 @@
+module AWD_FinalProject.ryanarmstrong.net
+
+go 1.18
+
+require github.com/julienschmidt/httprouter v1.3.0 // indirect
